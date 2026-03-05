ad5b8a498a579f8ff37422bfad47b9242ff911ca
Musharraf-Bubere <musharrafbubere007@gmail.com>
2026-03-05 19:35:09 +0530

    deployment files are added

---
+++ core/recommender.py
@@ -0,0 +1,28 @@
+from utils.path_utils import CONFIG_PATH
+import yaml
+
+
+with open(CONFIG_PATH / "config.yaml") as f:
+    THRESHOLD = yaml.safe_load(f)
+
+def generate_recommendations(risk_probability, student_input):
+    
+    observations = []
+    actions = []
+
+    if student_input["early_active_days"] == 0:
+        observations.append("No engagement in early course period")
+        actions.append("Start with introductory materials immediately.")
+
+    if student_input["early_click"] < THRESHOLD["early_engagement"]["min_early_click"]:
+        observations.append("Low early interaction with course content")
+        actions.append("Watch intro videos and explore course structure.")
+
+    if student_input["first_activity_day"] > 0:
+        observations.append("Late course start comapared to peers")
+        actions.append("Follow a structured 7-day recovery plan")
+
+    if risk_probability >= THRESHOLD["risk_levels"]["high"]:
+        actions.append("Schedule academic support or mentoring session.")
+
+    return observations, actions
